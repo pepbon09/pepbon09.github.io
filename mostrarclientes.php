c2e6ce64c8e18c42ff5831da3348c458206cd9b7
pepbon09 <55553691+pepbon09@users.noreply.github.com>
2020-02-21 18:05:46 +0100

    Create mostrarclientes.php

---
+++ mostrarclientes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Mis Clientes</title>
+            <style type="text/css">
+                body {
+                    background-color: #142850;
+                }
+            </style>
+        </head>
+        <body style="#142850">
+            <div style="text-align: center">
+                
+            </div>
+        </body>
